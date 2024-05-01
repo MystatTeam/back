390ca1424bffdeeea6f8d844682cf1f8e167e371
totskaiasonia <totskaiasonia@gmail.com>
2024-05-01 14:13:48 +0300

    update student model

--- src/models/StudentModel.ts
+++ src/models/StudentModel.ts
@@ -2,8 +2,18 @@ import { model, Document, Schema } from 'mongoose';
 
 // data type which must be provided from client for crud operation
 export interface IStudent {
-    username: string;
+    login: string;
     passwordHash: string;
+    name: string;
+    surname: string;
+    afterFatherName: string;
+    phoneNumber: string;
+    birthday: Date;
+    group: string;
+    userPhoto: {
+        data: Buffer,
+        contentType: string
+    };
     stats: {
         diamonds: number;
         coins: number;
@@ -19,29 +29,56 @@ export interface IStudentModel extends IStudent, Document {
 // Schema for mongoDB
 const StudentSchema: Schema = new Schema<IStudent>(
     {
-        username: {
+        login: {
             type: String,
             required: true,
             unique: true,
         },
         passwordHash: {
             type: String,
-            required: true
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        surname: {
+            type: String,
+            required: true,
+        },
+        afterFatherName: {
+            type: String,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        birthday: {
+            type: Date,
+            required: true,
+        },
+        group: {
+            type: String,
+            required: true,
+        },
+        userPhoto: {
+            data: Buffer,
+            contentType: String,
         },
         stats: {
             diamonds: {
                 type: Number,
-                default: 0
+                default: 0,
             },
-            coind: {
+            coins: {
                 type: Number,
                 default: 0,
             },
             rewards: {
                 type: Number,
-                default: 0
-            }
-        }
+                default: 0,
+            },
+        },
     },
     {
         timestamps: true,
